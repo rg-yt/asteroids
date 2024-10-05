c25a6374c1949208c5a58731bc60f39e38502952
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-05 18:38:54 -0400

    limit fps to reduce resource usage

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,7 @@ def main():
             return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
