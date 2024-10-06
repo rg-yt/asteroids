3b8c8ddbd03808b261bb15a8e33abf23d88598b0
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-06 17:12:07 -0400

    add draw method to shot

--- shot.py
+++ shot.py
@@ -1,5 +1,9 @@
 from circleshape import CircleShape
+import pygame
 
 class Shot(CircleShape):
   def __init__(self, x, y, radius):
     super().__init__(x, y, radius)
+
+  def draw(self, screen):
+    pygame.draw.circle(screen, "white", self.position, self.radius, 2)
