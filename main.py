deea67cc163955b37c19aaeb5d457436771866ff
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-06 16:09:41 -0400

    set up and assign groups for asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
 
 def main():
     pygame.init()
@@ -9,8 +10,10 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, drawable, updatable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
