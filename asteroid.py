213c4d1891b73f9b1bee034dba3249f8f1968dc9
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-06 18:54:09 -0400

    refactor asteroid split method

--- asteroid.py
+++ asteroid.py
@@ -3,6 +3,7 @@ import pygame
 from constants import *
 import random
 
+
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
     super().__init__(x, y, radius)
@@ -15,19 +16,19 @@ class Asteroid(CircleShape):
 
   def split(self):
     self.kill()
+    
     if self.radius <= ASTEROID_MIN_RADIUS:
       return
     
-    angle = random.uniform(20, 50)
+    a = self.velocity.rotate(random_angle)
+    b = self.velocity.rotate(-random_angle)
+    
+    random_angle = random.uniform(20, 50)
 
-    vector_a = self.velocity.rotate(angle)
-    vector_b = self.velocity.rotate(-angle)
-
-    new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
-    child1 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
-    child2 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
-
-    child1.velocity = vector_a * 1.2
-    child2.velocity = vector_b * 1.2
+    new_radius = self.radius - ASTEROID_MIN_RADIUS
+    asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+    asteroid.velocity = a * 1.2
+    asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+    asteroid.velocity = b * 1.2
     
     
