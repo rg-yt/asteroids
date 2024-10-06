bceabfb7f647686e7ba90833936413795525b638
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-06 15:48:51 -0400

    add player to new groups

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    player.containers = (updatable, drawable)
     
     while True:
         for event in pygame.event.get():
