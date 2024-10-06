9b38500a491599751c3b9badaed1e95a39a33f83
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-06 17:15:30 -0400

    update shot method

--- player.py
+++ player.py
@@ -41,5 +41,5 @@ class Player(CircleShape):
     self.position += forward * PLAYER_SPEED * dt
 
   def shoot(self):
-    shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+    shot = Shot(self.position.x, self.position.y)
     shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
