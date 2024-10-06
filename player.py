b59996326c83585fa05cf473be1f90a96fbfb96b
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-05 21:13:08 -0400

    add boiler update method to player

--- player.py
+++ player.py
@@ -18,5 +18,13 @@ class Player(CircleShape):
   def draw(self, screen):
     pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-  def rotate(dt):
+  def rotate(self, dt):
     self.rotation += PLAYER_TURN_SPEED * dt
+
+  def update(self, dt):
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_a]:
+      pass
+    if keys[pygame.K_d]:
+      pass
