2634aaa8e7c9b96badf561c6e82fada00bf57e60
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-06 16:17:08 -0400

    create asteroidfield object in main

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
     AsteroidField.containers = (updatable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     dt = 0
     
