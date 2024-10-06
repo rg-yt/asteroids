c53340fc0f0d01887215ef0fa8ab9909a64b5e6b
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-10-06 15:55:34 -0400

    update game loop to use new groups

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    player.containers = (updatable, drawable)
+
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
     
     while True:
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
             return
-        player.update(dt)
+        
+        for obj in updatable:
+           obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+           obj.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
