94f7cecf84cc9f0d2ba415a389e978ee19ac823a
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-01-02 16:15:01 -0500

    Created groups to manage more objects in game loop

--- main.py
+++ main.py
@@ -7,17 +7,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        
+        for entity in updatable:
+            entity.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        for entity in drawable:
+            entity.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
