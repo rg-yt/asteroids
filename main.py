58f339d1a3a6d8e3769ec6199a1a4fbddec4f02a
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-06-20 22:27:48 -0400

    Update main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+from constatnts import *
+
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
