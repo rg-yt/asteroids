1c3d5396dc4a225417b8489016cd14056c8b49be
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-06-23 15:19:24 -0400

    Make asteroids split when hit

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
 import pygame
-
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        split_angle = random.uniform(20,50)
+        angle1 = self.velocity.rotate(split_angle)
+        angle2 = self.velocity.rotate(-split_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = angle1 * 1.2
+        asteroid2.velocity = angle2 * 1.2
+
--- main.py
+++ main.py
@@ -33,6 +33,10 @@ def main():
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
 
         screen.fill("black")
         for sprite in drawable:
